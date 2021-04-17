c6414246fa1b5b58c1ae0c1e0278f86bea063c08
tanishqvyas <tanishqvyas069@gmail.com>
2021-04-17 19:34:02 +0530

    Home Page made Responsive

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -54,28 +54,28 @@ const HomePage = () => {
       </div>
 
       <div className={styles.upper_section_container}>
-        <div className={`${styles.upper_section} ${styles.section}`}>
+        <div id={styles.upper_section_one} className={`${styles.upper_section} ${styles.section}`}>
           <img className={styles.section_banner} src={BANNER_6} alt="Images" />
           <p className={styles.upper_section_info_ctnr}>
             Funding of <br /> <b>$150K+</b>
           </p>
         </div>
 
-        <div className={`${styles.upper_section} ${styles.section}`}>
+        <div id={styles.upper_section_two} className={`${styles.upper_section} ${styles.section}`}>
           <p className={styles.upper_section_info_ctnr}>
             Research and Market insights in focused areas
           </p>
           <img className={styles.section_banner} src={BANNER_7} alt="Images" />
         </div>
 
-        <div className={`${styles.upper_section} ${styles.section}`}>
+        <div id={styles.upper_section_three} className={`${styles.upper_section} ${styles.section}`}>
           <img className={styles.section_banner} src={BANNER_16} alt="Images" />
           <p className={styles.upper_section_info_ctnr}>
             Mentoring, connect to investors and customers
           </p>
         </div>
 
-        <div className={`${styles.upper_section} ${styles.section}`}>
+        <div id={styles.upper_section_four} className={`${styles.upper_section} ${styles.section}`}>
           <p className={styles.upper_section_info_ctnr}>
             Resources to get started with your plan quickly
           </p>
@@ -102,10 +102,10 @@ const HomePage = () => {
       </div>
 
       <div className={styles.section_heading}>our process</div>
-      <img src={STEP_1} />
-      <img src={STEP_2} />
-      <img src={STEP_3} />
-      <img src={STEP_4} />
+      <img id={styles.step_one} src={STEP_1} />
+      <img id={styles.step_two} src={STEP_2} />
+      <img id={styles.step_three} src={STEP_3} />
+      <img id={styles.step_four} src={STEP_4} />
       <div className={styles.section_heading}>what we offer</div>
       <div id={styles.what_we_offer} className={styles.section}>
         <WhatWeOfferCard
