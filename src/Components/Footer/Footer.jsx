8cb43eed070e4a142f882b7b2eb8c784ba060e77
tanishqvyas <tanishqvyas069@gmail.com>
2021-04-21 12:44:24 +0530

    footer Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,81 +4,108 @@ import styles from "./Footer.module.css";
 /* IMPORT VARIABLES */
 import { websiteBaseURL } from "../../Config/Config";
 import { linkedinURL, instagramURL } from "../../Data/Links";
+import { FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 
 /* IMPORT IMAGES HERE */
-import LOGO from "../../Images/Logos/PVL_LOGO_YELLOW.png";
-import linkedIcon from "../../Images/Icons/linkedin.svg";
-import instagramIcon from "../../Images/Icons/instagram.svg";
+import LOGO from "../../Images/Logos/PVL_LOGO_WHITE.png";
 
 const footerLinks = [
   {
-    "title": "about us",
-    "link":  websiteBaseURL  + "aboutus",
+    title: "about us",
+    link: websiteBaseURL + "aboutus",
   },
   {
-    "title": "the Team",
-    "link":  websiteBaseURL  + "team",
+    title: "the Team",
+    link: websiteBaseURL + "team",
   },
   {
-    "title": "contact us",
-    "link":  websiteBaseURL  + "contact",
+    title: "contact us",
+    link: websiteBaseURL + "contact",
   },
   {
-    "title": "portfolio",
-    "link":  websiteBaseURL  + "portfolio",
+    title: "portfolio",
+    link: websiteBaseURL + "portfolio",
   },
   {
-    "title": "Opportunities",
-    "link":  websiteBaseURL  + "opportunities",
+    title: "Opportunities",
+    link: websiteBaseURL + "opportunities",
   },
   {
-    "title": "resources",
-    "link":  websiteBaseURL  + "resources",
+    title: "resources",
+    link: websiteBaseURL + "resources",
   },
   {
-    "title": "apply",
-    "link":  websiteBaseURL  + "apply",
-  },
-];
-
-const footerIcons = [
-  {
-    "icon":  linkedIcon ,
-    "link":  linkedinURL ,
-  },
-  {
-    "icon":  instagramIcon ,
-    "link":  instagramURL ,
+    title: "apply",
+    link: websiteBaseURL + "apply",
   },
 ];
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo_container}>
-        <img className={styles.logo} src={LOGO} alt="Image" />
-      </div>
-      <div className={styles.info_container}>
-        <div className={styles.title}>PESU VENTURE LABS</div>
-        <h3 className={styles.tagline}>MAKE YOUR IDEAS SEE THE LIGHT</h3>
-        <div className={styles.link_container}>
-          {footerLinks.map((item) => (
+      <div id={styles.section_one}>
+        <div id={styles.logo_ctnr}>
+          <img id={styles.logo} src={LOGO} alt="Image" />
+          <div id={styles.logoname}>PESU VENTURE LABS</div>
+        </div>
+        <div className={styles.nav_container}>
+          {footerLinks.map((item, index) => (
             <a
               href={item.link}
               rel="noopener noreferrer"
-              className={styles.link_ele}
+              className={styles.nav_ele}
             >
               {item.title}
             </a>
           ))}
         </div>
-        <div className={styles.tagline} >Connect with us at</div>
-        <div className={styles.icons_container}>
-          {footerIcons.map((item, index) => (
-            <a target="_blank" rel="noopener noreferrer" href={item.link} className={styles.icon_wrapper}>
-                <img className={styles.icon} src={item.icon} alt="Icon" />
+      </div>
+
+      {/* <div id={styles.section_two}>
+        <div id={styles.section_two_heading}>Join the Waitlist!</div>
+
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="#"
+          className={`${"basic-button_one"}`}
+          id={styles.section_two_btn}
+        >
+          JOIN
+        </a>
+      </div> */}
+
+      <div id={styles.section_three}>
+        <div id={styles.section_three_para_ctnr}>
+          <p className={styles.para}>
+            12th Floor CIE Office, B Block <br />
+            PES University, Outer Ring Road
+          </p>
+          <p className={styles.para}>
+            Privacy Policy- Terms & Conditions- Legal <br />© 2021 PVL All
+            Rights Reserved
+          </p>
+        </div>
+
+        <div id={styles.connect_with_us_ctnr}>
+          <div id={styles.connect_with_us}>Connect with us!</div>
+          <div className={styles.icons_container}>
+            <a
+              rel="noopener noreferrer"
+              href={instagramURL}
+              className={styles.icon_wrapper}
+            >
+              <FaInstagram className={styles.icon} />
             </a>
-          ))}
+
+            <a
+              rel="noopener noreferrer"
+              href={linkedinURL}
+              className={styles.icon_wrapper}
+            >
+              <FaLinkedin className={styles.icon} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
