1def356f2c462fbfab2017b63b00ede7ec8334b5
santoshbishnoi <santoshsbishnoi6@gmail.com>
2021-05-01 23:33:34 +0530

    production release 2

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,7 @@
 /* DEVELOPMENT  CONFIGURATION*/
-export const websiteBaseURL = "http://localhost:3000/";
-export const apiURL = "http://localhost:5000"
+// export const websiteBaseURL = "http://localhost:3000/";
+// export const apiURL = "http://localhost:5000"
 
 /* PRODUCTION  CONFIGURATION*/
-// export const websiteBaseURL = "https://pesuventurelab.github.io/pvl-frontend/#/";
+export const websiteBaseURL = "https://pesuventurelab.github.io/pvl-frontend/#/";
+export const apiURL = "http://localhost:5000"
