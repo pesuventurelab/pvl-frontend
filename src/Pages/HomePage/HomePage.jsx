ede84697a1a1e7e777b62c772323d7d9b71ab896
santoshbishnoi <santoshsbishnoi6@gmail.com>
2021-05-04 01:07:36 +0530

    enabled cors on apigateway

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -396,7 +396,7 @@ const HomePage = () => {
         />
       </div>
 
-      {/* <ScrollAnimation duration="0.9" animateIn="fadeInUp">
+      <ScrollAnimation duration="0.9" animateIn="fadeInUp">
         <button
           onClick={() => toggleEmailContainer(true)}
           id={styles.contact_us_btn}
@@ -404,7 +404,7 @@ const HomePage = () => {
         >
           Subscribe to Newsletter
         </button>
-      </ScrollAnimation> */}
+      </ScrollAnimation>
       <div ref={emailCtnrREF} className={styles.email_container}>
         <IoIosArrowDown
           onClick={() => toggleEmailContainer(false)}
